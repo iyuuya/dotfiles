51f8563385d97d850889eb61cb1b05dcf26867f1
iyuuya <i.yuuya@gmail.com>
2021-12-31 23:51:58 +0900

    nvim: manage plugins with packer.nvim

---
+++ nvim/.config/nvim/lua/vento/plugins/init.lua
@@ -0,0 +1,25 @@
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print("Installing packer.nvim")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path
+  })
+  vim.api.nvim_command("packadd packer.nvim")
+end
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+packer.init({})
+
+return packer.startup(function(use)
+  use("wbthomason/packer.nvim")
+end)
