d5e3f0e17242f67257a8f5ef178a63c4e59dd19f
iyuuya <i.yuuya@gmail.com>
2021-12-31 22:40:08 +0900

    alacritty: introduce alacritty configuration

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
-cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux
+cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux alacritty
 sh $SCRIPT_DIR/depends/bootstrap.sh
