ca7c99593518d275914e8fa411d12236b99c4be5
iyuuya <i.yuuya@gmail.com>
2022-01-02 11:57:14 +0900

    feat(nvim): setup lsp

--- nvim/.config/nvim/lua/vento/options/init.lua
+++ nvim/.config/nvim/lua/vento/options/init.lua
@@ -3,4 +3,5 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.foldlevelstart = 99
 vim.opt.termguicolors = true
--- nvim/.config/nvim/lua/vento/plugins/init.lua
+++ nvim/.config/nvim/lua/vento/plugins/init.lua
@@ -2,13 +2,13 @@ local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.n
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   print("Installing packer.nvim")
-  packer_boostrap = vim.fn.system({
+  PACKER_BOOSTRAP = vim.fn.system({
     "git",
     "clone",
     "--depth",
     "1",
     "https://github.com/wbthomason/packer.nvim",
-    install_path
+    install_path,
   })
   vim.api.nvim_command("packadd packer.nvim")
 end
@@ -71,7 +71,7 @@ return packer.startup(function(use)
 
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-    end
+    end,
   })
   use("nvim-treesitter/nvim-treesitter-textobjects")
 
@@ -81,11 +81,74 @@ return packer.startup(function(use)
     requires = { "rktjmp/lush.nvim" },
     config = function()
       vim.opt.background = "dark"
-      vim.cmd[[colorscheme gruvbox]]
+      vim.cmd([[colorscheme gruvbox]])
     end,
   })
 
-  if packer_boostrap then
+  -- LSP
+  use({
+    "neovim/nvim-lspconfig",
+    config = function()
+      local luadev = require("lua-dev")
+      local lspconfig = require("lspconfig")
+
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+        -- Enable completion triggered by <c-x><c-o>
+        buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+        -- Mappings.
+        local opts = { noremap=true, silent=true }
+
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+        buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+        buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+        buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+        buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+        buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+        buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+        buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+        buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+        buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+        buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+      end
+
+      lspconfig.sumneko_lua.setup(
+        luadev.setup({
+          runtime_path = true,
+          lspconfig = {
+            on_attach = on_attach,
+          },
+        })
+      )
+
+      -- Use a loop to conveniently call "setup" on multiple servers and
+      -- map buffer local keybindings when the language server attaches
+      -- local servers = { "rust_analyzer", "tsserver" }
+      -- for _, lsp in ipairs(servers) do
+      --   lspconfig[lsp].setup {
+      --     on_attach = on_attach,
+      --     flags = {
+      --       debounce_text_changes = 150,
+      --     }
+      --   }
+      -- end
+    end
+  })
+  use("folke/lua-dev.nvim")
+
+  if PACKER_BOOSTRAP then
     require("packer").sync()
   end
 end)
