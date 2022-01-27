64ff0d90e5a22c0b2ca5725523b9aa3975af4b5c
iyuuya <i.yuuya@gmail.com>
2022-01-27 10:38:51 +0900

    add ranger config

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
-cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux alacritty
+cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux alacritty ranger
 sh $SCRIPT_DIR/depends/bootstrap.sh
