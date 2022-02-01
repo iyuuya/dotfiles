b2fa3c6dcdff5c87edd4ac5adb24deaeb7fbc7cf
iyuuya <i.yuuya@gmail.com>
2022-02-01 10:42:12 +0900

    add typescript-langauge-server

--- depends/neovim.sh
+++ depends/neovim.sh
@@ -4,7 +4,7 @@ if [ -d $HOME/.asdf ]; then
   source "$HOME/.asdf/asdf.sh"
   
   gem install neovim
-  npm i -g neovim
+  npm i -g neovim typescript-language-server
   pip install --upgrade pip
   pip install pynvim ranger-fm cmake-language-server
   asdf reshim python
