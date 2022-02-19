ff9bb7ff92b9d463b884f1f40992eed263438b3b
iyuuya <i.yuuya@gmail.com>
2022-02-19 18:36:25 +0900

    add solargraph

--- depends/neovim.sh
+++ depends/neovim.sh
@@ -3,7 +3,7 @@
 if [ -d $HOME/.asdf ]; then
   source "$HOME/.asdf/asdf.sh"
   
-  gem install neovim
+  gem install neovim solargprah
   npm i -g neovim typescript-language-server
   pip install --upgrade pip
   pip install pynvim ranger-fm cmake-language-server
