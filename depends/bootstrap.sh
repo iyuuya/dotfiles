9e70792efff820ac44b104c1bf4d235dc47b531d
iyuuya <i.yuuya@gmail.com>
2022-08-19 18:12:46 +0900

    feat: add debian bootstrap

--- depends/bootstrap.sh
+++ depends/bootstrap.sh
@@ -5,6 +5,13 @@ cd $SCRIPT_DIR
 
 if [[ $OSTYPE == "darwin"* ]]; then
   brew bundle
+elif [[ $(uname -a) == *"Debian"* ]]; then
+  sudo apt-get update && \
+  sudo apt-get install -y --no-install-recommends \
+      automake autoconf bison build-essential libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev make \
+    libreadline6-dev zlib1g-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev && \
+  sudo apt-get clean && \
+  sudo rm -rf /var/lib/apt/lists/*
 fi
 
 sh $SCRIPT_DIR/rust.sh
