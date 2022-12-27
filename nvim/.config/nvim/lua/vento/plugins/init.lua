797392a9b260bb3f51afe12ee5e760f874993f70
iyuuya <i.yuuya@gmail.com>
2022-12-28 01:16:57 +0900

    feat: tomorrow-night-bright

--- nvim/.config/nvim/lua/vento/plugins/init.lua
+++ nvim/.config/nvim/lua/vento/plugins/init.lua
@@ -145,23 +145,29 @@ return packer.startup(function(use)
   use("pocke/rbs.vim")
 
   -- Colorscheme
-  -- use({
-  --   "ellisonleao/gruvbox.nvim",
-  --   requires = { "rktjmp/lush.nvim" },
-  --   config = function()
-  --     vim.opt.background = "dark"
-  --     vim.cmd([[
-  --       colorscheme gruvbox
-  --       hi Normal ctermbg=NONE guibg=NONE
-  --     ]])
-  --   end,
-  -- })
+  use({
+    "ellisonleao/gruvbox.nvim",
+    requires = { "rktjmp/lush.nvim" },
+    -- config = function()
+    --   vim.opt.background = "dark"
+    --   vim.cmd([[
+    --     colorscheme gruvbox
+    --     hi Normal ctermbg=NONE guibg=NONE
+    --   ]])
+    -- end,
+  })
   use({
     "EdenEast/nightfox.nvim",
+    -- config = function()
+    --   vim.cmd([[
+    --     colorscheme nightfox
+    --   ]])
+    -- end
+  })
+  use({
+    "chriskempson/vim-tomorrow-theme",
     config = function()
-      vim.cmd([[
-        colorscheme nightfox
-      ]])
+      vim.cmd([[colorscheme Tomorrow-Night-Bright]])
     end
   })
   use({
