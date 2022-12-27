dec3e7db67c5dcf8cf53a77b555ec90b7449da8b
iyuuya <i.yuuya@gmail.com>
2022-12-27 16:18:06 +0900

    build: bumpup processors

--- depends/neovim.sh
+++ depends/neovim.sh
@@ -4,7 +4,7 @@ if [ -d $HOME/.asdf ]; then
   source "$HOME/.asdf/asdf.sh"
   
   gem install neovim solargprah
-  npm i -g neovim typescript-language-server
+  npm i -g neovim typescript-language-server graphql-language-service-cli
   pip install --upgrade pip
   pip install pynvim ranger-fm cmake-language-server
   asdf reshim python
