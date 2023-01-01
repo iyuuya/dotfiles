75825b5e3c766230dd88d6a13be97c0bfa056264
iyuuya <i.yuuya@gmail.com>
2023-01-01 13:19:13 +0900

    chore: disable build alacritty

--- depends/alacritty.sh
+++ depends/alacritty.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
 if [[ $OSTYPE == "darwin"* ]]; then
-  if [ ! -d /Applications/Alacritty.app ]; then
-    if ! command -V cargo > /dev/null 2>&1; then
-      source "$HOME/.cargo/env"
-    fi
+  # if [ ! -d /Applications/Alacritty.app ]; then
+  #   if ! command -V cargo > /dev/null 2>&1; then
+  #     source "$HOME/.cargo/env"
+  #   fi
 
-    git clone --depth 1 https://github.com/alacritty/alacritty ~/.local/src/github.com/alacritty/alacritty
-    cd ~/.local/src/github.com/alacritty/alacritty \
-      && make app \
-      && cp -r target/release/osx/Alacritty.app /Applications
-  fi
+  #   git clone --depth 1 https://github.com/alacritty/alacritty ~/.local/src/github.com/alacritty/alacritty
+  #   cd ~/.local/src/github.com/alacritty/alacritty \
+  #     && make app \
+  #     && cp -r target/release/osx/Alacritty.app /Applications
+  # fi
 
   if ! fc-list : family | grep 'SauceCodePro Nerd Font' > /dev/null 2>&1; then
     brew tap homebrew/cask-fonts
