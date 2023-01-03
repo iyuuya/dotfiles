a42834db4b7b121599b67e188abe48266ef45084
iyuuya <i.yuuya@gmail.com>
2023-01-03 14:39:01 +0900

    feat(nvim): add clang-format

--- nvim/.config/nvim/lua/vento/plugins/cmp.lua
+++ nvim/.config/nvim/lua/vento/plugins/cmp.lua
@@ -50,6 +50,8 @@ local mycmp = {
             null_ls.builtins.code_actions.gitsigns,
 
             null_ls.builtins.formatting.rustfmt,
+
+            null_ls.builtins.formatting.clang_format,
           },
           on_attach = function(client, bufnr)
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
