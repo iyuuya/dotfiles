d5cf89e5debd9872c8083e89d03b9042238d03cc
iyuuya <i.yuuya@gmail.com>
2024-05-01 10:07:09 +0900

    tweek

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
+PATH=/opt/ $PATH
+
 mkdir -p $HOME/.local
 cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux alacritty ranger wezterm local_bin hyper
 zsh $SCRIPT_DIR/depends/bootstrap.sh
