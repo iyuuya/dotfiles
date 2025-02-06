5d3ace16885dc02e8a6199b66bb0619f394fb9f6
iyuuya <i.yuuya@gmail.com>
2025-02-07 08:48:24 +0900

    chore: remove check asdf

--- depends/neovim.sh
+++ depends/neovim.sh
@@ -1,11 +1,6 @@
 #!/bin/zsh
 
-if [ -d $HOME/.asdf ]; then
-  source "$HOME/.asdf/asdf.sh"
-  
-  gem install neovim solargprah
-  npm i -g neovim typescript-language-server graphql-language-service-cli
-  pip install --upgrade pip
-  pip install pynvim ranger-fm cmake-language-server
-  asdf reshim python
-fi
+gem install neovim solargprah
+npm i -g neovim typescript-language-server graphql-language-service-cli
+pip install --upgrade pip
+pip install pynvim ranger-fm cmake-language-server
--- depends/tmuxinator.sh
+++ depends/tmuxinator.sh
@@ -1,7 +1,3 @@
 #!/bin/zsh
 
-if [ -d $HOME/.asdf ]; then
-  source "$HOME/.asdf/asdf.sh"
-
-  gem install tmuxinator
-fi
+gem install tmuxinator
