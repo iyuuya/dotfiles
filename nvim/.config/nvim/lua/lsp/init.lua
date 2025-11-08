4d696168a35f0b8bc202bc595f0a8474f6adcee6
iyuuya <i.yuuya@gmail.com>
2025-11-09 08:53:22 +0900

    feat(nvim): use <leader>

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -55,7 +55,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 			-- Add normal-mode keymappings for signature help
 			if client:supports_method("textDocument/signatureHelp") then
-				map("n", "<C-s>", function()
+				map("n", "<leader>ls", function()
 					vim.lsp.buf.signature_help()
 				end, { desc = "Trigger lsp signature help" })
 			end
