f8538a0c55c9c96d81395c26ab822e9c4150409f
iyuuya <i.yuuya@gmail.com>
2025-11-09 09:15:52 +0900

    feat(nvim): core

--- nvim/.config/nvim/lua/plugins/spec1.lua
+++ nvim/.config/nvim/lua/plugins/spec1.lua
@@ -1,5 +1,30 @@
 return {
 	--------------------------------------------------------------------------------
+	--- core
+	--------------------------------------------------------------------------------
+	{ "nvim-lua/plenary.nvim" },
+	{ "folke/snacks.nvim", opts = {} },
+	{
+		"folke/noice.nvim",
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		},
+	},
+	--------------------------------------------------------------------------------
 	--- which-key
 	--------------------------------------------------------------------------------
 	{
@@ -30,29 +55,4 @@ return {
 			},
 		},
 	},
-
-	{ "nvim-lua/plenary.nvim" },
-	{ "folke/snacks.nvim", opts = {} },
-	{ "s1n7ax/nvim-window-picker", opts = {} },
-	{
-		"folke/noice.nvim",
-		opts = {
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-			},
-			-- you can enable a preset for easier configuration
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
-			},
-		},
-	},
-	{ "folke/snacks.nvim", opts = {} },
 }
