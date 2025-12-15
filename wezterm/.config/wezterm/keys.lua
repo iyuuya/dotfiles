5b01249df694fca9da742b4ccfb8f5565dcb6366
iyuuya <i.yuuya@gmail.com>
2025-12-15 09:23:56 +0900

    feat: select livemode, notemode and colorscheme

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -6,20 +6,36 @@ pixelmode.font = wz.font("PixelMplus12")
 pixelmode.color_scheme = "iceberg-dark"
 pixelmode.font_size = 20.0
 
+local livemode = wz.config_builder()
+livemode.font = wz.font("JetBrains Mono")
+livemode.color_scheme = "Catppuccin Mocha"
+livemode.font_size = 16.0
+
+local notemode = wz.config_builder()
+notemode.font = wz.font("JetBrains Mono")
+-- notemode.color_scheme = "Builtin Solarized Light"
+notemode.color_scheme = "Everforest Light Hard (Gogh)"
+notemode.font_size = 18.0
+notemode.window_background_opacity = 1
+notemode.macos_window_background_blur = 0
+notemode.enable_scroll_bar = false
+
 local configs = {
 	default = {},
 	pixelmode = pixelmode,
+	livemode = livemode,
+	notemode = notemode,
 }
 
 local launch_menu = {
-  {
-    label = "iarch",
-    args = { "/Users/yuya-ito/.orbstack/bin/orb", "run", "-m", "iarch" },
-  },
-  {
-    label = "iubuntu",
-    args = { "/Users/yuya-ito/.orbstack/bin/orb", "run", "-m", "iubuntu" },
-  },
+	{
+		label = "iarch",
+		args = { "/Users/yuya-ito/.orbstack/bin/orb", "run", "-m", "iarch" },
+	},
+	{
+		label = "iubuntu",
+		args = { "/Users/yuya-ito/.orbstack/bin/orb", "run", "-m", "iubuntu" },
+	},
 }
 
 --   -- key bindings
@@ -28,6 +44,33 @@ local launch_menu = {
 --     { key = "2", mods = "CMD",       action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
 --     { key = "d", mods = "CMD|SHIFT", action = "ShowLauncher" },
 --   }
+
+local colors = {}
+local function make_pallete(ansi)
+	local SGR = "\x1b["
+	local str = ""
+	for _, v in ipairs(ansi) do
+		local r = tonumber(v:sub(2, 3), 16)
+		local g = tonumber(v:sub(4, 5), 16)
+		local b = tonumber(v:sub(6, 7), 16)
+		local add = string.format("%s48;2;%d;%d;%dm ", SGR, r, g, b)
+		str = str .. add
+	end
+	return str
+end
+for k, v in pairs(wz.color.get_builtin_schemes()) do
+	table.insert(colors, {
+		id = k,
+		label = wz.format({
+			{ Text = make_pallete(v.ansi) },
+			{ Text = make_pallete(v.brights) },
+			{ Text = "\x1b[0m " },
+			{ Foreground = { Color = v.foreground } },
+			{ Background = { Color = v.background } },
+			{ Text = k },
+		}),
+	})
+end
 
 return {
 	leader = {
@@ -39,7 +82,7 @@ return {
 		{
 			mods = "LEADER",
 			key = "d",
-			action = wz.action.ShowDebugOverlay,
+			action = act.ShowDebugOverlay,
 		},
 		{
 			key = "d",
@@ -49,30 +92,32 @@ return {
 		{
 			mods = "LEADER",
 			key = "l",
-      action = act.InputSelector {
-        title = "Launch…",
-        choices = (function()
-          local choices = {}
-          for _, item in ipairs(launch_menu) do
-            table.insert(choices, { label = item.label, id = item.label })
-          end
-          return choices
-        end)(),
-        action = wz.action_callback(function(window, pane, id, label)
-          if not id then return end
+			action = act.InputSelector({
+				title = "Launch…",
+				choices = (function()
+					local choices = {}
+					for _, item in ipairs(launch_menu) do
+						table.insert(choices, { label = item.label, id = item.label })
+					end
+					return choices
+				end)(),
+				action = wz.action_callback(function(window, pane, id, label)
+					if not id then
+						return
+					end
 
-          for _, item in ipairs(launch_menu) do
-            if item.label == id then
-              wz.mux.spawn_window({ args = item.args })
-            end
-          end
-        end),
-      }
+					for _, item in ipairs(launch_menu) do
+						if item.label == id then
+							wz.mux.spawn_window({ args = item.args })
+						end
+					end
+				end),
+			}),
 		},
 		{
 			mods = "LEADER",
 			key = "t",
-			action = wz.action.ShowLauncherArgs({
+			action = act.ShowLauncherArgs({
 				flags = "FUZZY|TABS",
 				title = "Select tab",
 			}),
@@ -80,15 +125,15 @@ return {
 		{
 			mods = "LEADER",
 			key = "$",
-			action = wz.action.PromptInputLine({
+			action = act.PromptInputLine({
 				description = "(wezterm) Set workspace title:",
 				action = wz.action_callback(function(win, pane, line)
 					if line then
 						wz.mux.rename_workspace(wz.mux.get_active_workspace(), line)
-            local tab = pane:tab()
-            if tab then
-              tab:set_title(line)
-            end
+						local tab = pane:tab()
+						if tab then
+							tab:set_title(line)
+						end
 					end
 				end),
 			}),
@@ -108,11 +153,33 @@ return {
 				title = "Select Configuration Profile",
 				choices = {
 					{ label = "default", id = "default" },
-					-- This is the second entry
 					{ label = "pixelmode", id = "pixelmode" },
+					{ label = "livemode", id = "livemode" },
+					{ label = "notemode", id = "notemode" },
 				},
 			}),
 		},
+		{
+			mods = "LEADER",
+			key = "C",
+			action = wz.action_callback(function(win, pane)
+				win:perform_action(
+					wz.action.InputSelector({
+						title = "color_scheme",
+						choices = colors,
+						action = wz.action_callback(function(inner_window, _, id)
+							if id then
+								inner_window:set_config_overrides({
+									color_scheme = id,
+								})
+							end
+						end),
+						fuzzy = true,
+					}),
+					pane
+				)
+			end),
+		},
 	},
-  launch_menu = launch_menu,
+	launch_menu = launch_menu,
 }
