ab9555b54910894deb1493ed3e5612f35249ed8c
iyuuya <i.yuuya@gmail.com>
2026-01-06 13:42:57 +0900

    chore: tuning local_bin

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
 PATH=/opt/ $PATH
 
-mkdir -p $HOME/.local
-cd $SCRIPT_DIR && stow -t $HOME zsh nvim lvim tmux ranger wezterm mise local_bin
+mkdir -p $HOME/.local/bin
+cd $SCRIPT_DIR && stow -t $HOME zsh nvim tmux ranger wezterm mise local_bin
 zsh $SCRIPT_DIR/depends/bootstrap.sh
