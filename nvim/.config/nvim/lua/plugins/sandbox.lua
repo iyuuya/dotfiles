55df3ad2597dfa3dfafa953a75f87ad08cbc987e
iyuuya <i.yuuya@gmail.com>
2026-02-16 09:38:59 +0900

    feat: disable ziit

--- nvim/.config/nvim/lua/plugins/sandbox.lua
+++ nvim/.config/nvim/lua/plugins/sandbox.lua
@@ -1,14 +1,2 @@
 return {
-	{
-		"0PandaDEV/ziit-neovim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("ziit").setup({
-				api_key = "4d69114f-1d5e-4e69-b04c-cd86b838a2d5",
-				base_url = 'https://app.ziit.orb.local', -- optional, defaults to https://ziit.app
-				enabled = true,
-				heartbeat_interval = 30,
-			})
-		end,
-	},
 }
